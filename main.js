55dd5e80e098a2d4c5a55c8afd850d97afb14064
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-02 10:23:10 +0200

    dev: added loop

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("coucou");
+const samplesGroup = document.querySelector("svg g.samples");
+if (samplesGroup === null) {
+  throw new Error("oups");
+}
+
+for (let i = 0; i < 10; i++) {
+  const elt = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  elt.setAttribute("r", "1");
+  elt.setAttribute("cx", "23");
+  elt.setAttribute("cy", "32");
+
+  console.dir(elt);
+  samplesGroup.appendChild(elt);
+}
